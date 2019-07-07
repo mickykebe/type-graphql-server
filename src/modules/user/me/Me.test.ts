ce4b1a9419c1974af176c65472040a5b349c81be
mickykebe <mickykebe@gmail.com>
2019-07-07 20:57:20 +0300

    Lesson 10(Testing Resolvers)

--- src/modules/user/me/Me.test.ts
+++ src/modules/user/me/Me.test.ts
@@ -14,9 +14,9 @@ afterAll(async () => {
   await conn.close();
 });
 
-const registerMutation = `
-  mutation Register($data: RegisterInput!) {
-    register(data: $data) {
+const meQuery = `
+  {
+    me {
       id
       firstName
       lastName
@@ -26,34 +26,42 @@ const registerMutation = `
   }
 `;
 
-describe("Register", () => {
-  it("create user", async () => {
-    const user = {
+describe("Me", () => {
+  it("get user", async () => {
+    const user = await User.create({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       email: faker.internet.email(),
       password: faker.internet.password()
-    };
+    }).save();
 
     const response = await gCall({
-      source: registerMutation,
-      variableValues: {
-        data: user
-      }
+      source: meQuery,
+      userId: user.id
     });
+
     expect(response).toMatchObject({
       data: {
-        register: {
+        me: {
+          id: `${user.id}`,
           firstName: user.firstName,
           lastName: user.lastName,
+          name: `${user.firstName} ${user.lastName}`,
           email: user.email
         }
       }
     });
+  });
 
-    const dbUser = await User.findOne({ where: { email: user.email } });
-    expect(dbUser).toBeDefined();
-    expect(dbUser!.confirmed).toBeFalsy();
-    expect(dbUser!.firstName).toBe(user.firstName);
-  }, 15000);
+  it("return null", async () => {
+    const response = await gCall({
+      source: meQuery
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        me: null
+      }
+    });
+  });
 });
